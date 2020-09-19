7a278a37257e2260279bbf5a476a78b8b51f261b
jmweed <65138977+jmweed@users.noreply.github.com>
2020-09-19 16:02:49 -0500

    set up package json

---
+++ nine19.js
@@ -0,0 +1,12 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+	console.log("Welcome a new player!");
+	io.sockets.emit('name', type);
+});
